d4acf6c27092f7c3bc1c42722395e3c24c998649
vinuth <vinuthsriarampath@outlook.com>
2025-03-28 07:40:05 +0530

    refactor : remove unused account status methods from UserPrinciple class

--- src/main/java/edu/vinu/model/UserPrinciple.java
+++ src/main/java/edu/vinu/model/UserPrinciple.java
@@ -47,21 +47,6 @@ public class UserPrinciple implements UserDetails, Serializable {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
     public boolean isEnabled() {
         return !userEntity.isDisabled();
     }
