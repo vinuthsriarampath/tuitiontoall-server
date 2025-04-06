881323af542ac53f58d299a6fb3c9c630b866b26
vinuth <vinuthsriarampath@outlook.com>
2025-04-06 10:32:15 +0530

    chore : remove unused imports

--- src/main/java/edu/vinu/config/SwaggerConfig.java
+++ src/main/java/edu/vinu/config/SwaggerConfig.java
@@ -13,10 +13,10 @@
 
 package edu.vinu.config;
 
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
