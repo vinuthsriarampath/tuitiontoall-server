63faaa17f206411bfc49329bde9b97939bf82c55
vinuth <vinuthsriarampath@outlook.com>
2025-11-24 11:53:28 +0530

    feat: add repository interfaces for Institute, Role, Student, and Teacher entities and moved all the methods related to Institute, Teacher, Student from UserRepository

---
+++ src/main/java/edu/vinu/repository/StudentRepository.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.repository;
+
+import edu.vinu.entity.user_entities.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity,Long> {
+
+    @Query(value = "SELECT s.* FROM student s JOIN users u ON u.id = s.user_id WHERE u.is_disabled = false",nativeQuery = true)
+    List<StudentEntity> getAllStudents();
+
+    @Query(value = "SELECT s.* from student s JOIN users u ON u.id = s.user_id WHERE LOWER(s.first_name) LIKE LOWER(CONCAT(:firstName, '%')) AND u.is_disabled = false",nativeQuery = true)
+    List<StudentEntity> getStudentsByFirstNameLike(@Param("firstName") String firstName);
+
+    @Query(value = "SELECT s.* FROM student s JOIN users u ON u.id = s.user_id WHERE u.email = :email AND u.is_disabled = false",nativeQuery = true)
+    Optional<StudentEntity> findStudentByEmail(@Param("email") String email);
+}
--- src/main/java/edu/vinu/repository/UserRepository.java
+++ src/main/java/edu/vinu/repository/UserRepository.java
@@ -13,52 +13,16 @@
 
 package edu.vinu.repository;
 
-import edu.vinu.entity.user_entities.InstituteEntity;
-import edu.vinu.entity.user_entities.StudentEntity;
-import edu.vinu.entity.user_entities.TeacherEntity;
 import edu.vinu.entity.user_entities.UserEntity;
-import edu.vinu.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long>{
     UserEntity findByEmail(String username);
 
-    @Query("SELECT i FROM InstituteEntity i WHERE i.email = :email AND i.isDisabled = false")
-    InstituteEntity findInstituteByEmail(@Param("email") String email);
-
-    @Query("SELECT s FROM StudentEntity s WHERE s.email = :email AND s.isDisabled = false")
-    StudentEntity findStudentByEmail(@Param("email") String email);
-
-    @Query("SELECT t FROM TeacherEntity t WHERE t.email = :email AND t.isDisabled = false")
-    TeacherEntity findTeacherByEmail(@Param("email") String email);
-
     boolean existsByEmail(String email);
-
-    @Query("SELECT s from StudentEntity s WHERE LOWER(s.firstName) LIKE LOWER(CONCAT(:firstName, '%')) AND s.isDisabled = false")
-    List<StudentEntity> getStudentsByFirstNameLike(@Param("firstName") String firstName);
-
-    @Query("SELECT t FROM TeacherEntity t WHERE LOWER(t.firstName) LIKE LOWER(CONCAT(:firstName, '%')) AND t.isDisabled = false")
-    List<TeacherEntity> getTeachersByFirstNameLike(@Param("firstName") String firstName);
-
-    List<UserEntity> findAllByRole(Role role);
-
-    @Query("SELECT i FROM InstituteEntity i WHERE i.role = 'ROLE_INSTITUTE' AND i.isDisabled = false")
-    List<InstituteEntity> getAllInstitutes();
-
-    @Query("SELECT t FROM TeacherEntity t WHERE t.role = 'ROLE_TEACHER' AND t.isDisabled = false")
-    List<TeacherEntity> getAllTeachers();
-
-    @Query("SELECT s FROM StudentEntity s WHERE s.role = 'ROLE_STUDENT' AND s.isDisabled = false")
-    List<StudentEntity> getAllStudents();
-
-    @Query("SELECT i FROM InstituteEntity i WHERE LOWER(i.instituteName) LIKE LOWER(CONCAT('%', :instituteName, '%')) AND i.isDisabled = false")
-    List<InstituteEntity> findByInstituteName(@Param("instituteName") String instituteName);
 
     @Query("SELECT u.isDisabled From UserEntity u WHERE u.email = :email")
     Boolean isUserDisabledByEmail(String email);
