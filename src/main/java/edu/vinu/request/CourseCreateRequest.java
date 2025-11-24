691e3cac788c814f2488752dc98e6fcec95ef175
vinuth <vinuthsriarampath@outlook.com>
2025-11-24 12:10:24 +0530

    refactor: optimize imports in CourseCreateRequest

--- src/main/java/edu/vinu/request/CourseCreateRequest.java
+++ src/main/java/edu/vinu/request/CourseCreateRequest.java
@@ -15,7 +15,10 @@ package edu.vinu.request;
 
 
 import edu.vinu.enums.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
