610f03d23e4fa59d322db2331e3ad0c614efd4c4
vinuth <vinuthsriarampath@outlook.com>
2026-04-26 11:05:53 +0530

    feat: add pagination support for applications by teacher vacancy ID

--- src/main/java/edu/vinu/repository/ApplicationRepository.java
+++ src/main/java/edu/vinu/repository/ApplicationRepository.java
@@ -14,6 +14,9 @@
 package edu.vinu.repository;
 
 import edu.vinu.entity.ApplicationEntity;
+import edu.vinu.repository.projection.ApplicationProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -23,11 +26,41 @@ import org.springframework.stereotype.Repository;
 public interface ApplicationRepository extends JpaRepository<ApplicationEntity, Long> {
 
     @Query(value = """ 
-    SELECT EXISTS(
-        SELECT 1
-        FROM applications a
-        WHERE a.teacher_id = :teacherId AND a.teacher_vacancy_id = :vacancyId
+            SELECT EXISTS(
+                SELECT 1
+                FROM applications a
+                WHERE a.teacher_id = :teacherId AND a.teacher_vacancy_id = :vacancyId
+            )
+            """, nativeQuery = true)
+    Integer isUserAlreadyApplied(@Param("teacherId") Long teacherId, @Param("vacancyId") Long vacancyId);
+
+
+    @Query(value = """ 
+            SELECT
+                                    a.id AS id,
+                                    a.teacher_vacancy_id AS teacherVacancyId,
+                                    a.status AS status,
+                                    a.applied_date AS appliedDate,
+                                    a.last_modified_date AS lastModifiedDate,
+                                    u.id AS userId,
+                                    u.email AS email,
+                                    u.contact AS contact,
+                                    u.dp AS dp,
+                                    u.address AS address,
+                                    t.first_name AS firstName,
+                                    t.last_name AS lastName,
+                                    t.dob AS dob
+                                    FROM applications a
+                                    JOIN teacher t ON a.teacher_id = t.id
+                                    JOIN users u ON t.user_id = u.id
+                                    WHERE a.teacher_vacancy_id = :vacancyId
+            """,
+            countQuery = """
+                    SELECT COUNT(*)
+                    FROM applications a
+                    WHERE a.teacher_vacancy_id = :vacancyId
+                    """,
+            nativeQuery = true
     )
-    """, nativeQuery = true)
-    Integer isUserAlreadyApplied(@Param("teacherId") Long teacherId,@Param("vacancyId") Long vacancyId);
+    Page<ApplicationProjection> findAllByTeacherVacancy_Id(@Param("vacancyId") Long teacherVacancyId, Pageable pageable);
 }
