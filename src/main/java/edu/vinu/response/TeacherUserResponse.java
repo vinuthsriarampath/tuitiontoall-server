45f8e7089ec9c90f0d15e8eaeb82ef7f61aeed63
vinuth <vinuthsriarampath@outlook.com>
2026-04-26 11:05:16 +0530

    feat: add TeacherUserResponse class for teacher user data representation

---
+++ src/main/java/edu/vinu/response/TeacherUserResponse.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.response;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TeacherUserResponse {
+    private Long userId;
+    private String email;
+    private String contact;
+    private String dp;
+    private String address;
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+}
