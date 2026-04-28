dd1323cf004f6c6f0c86b8ae72160b879469f51c
vinuth <vinuthsriarampath@outlook.com>
2026-04-28 20:15:50 +0530

    feat: add InstituteTeacherProjection interface and update InstituteTeacherRepository for projection-based queries

--- src/main/java/edu/vinu/repository/InstituteTeacherRepository.java
+++ src/main/java/edu/vinu/repository/InstituteTeacherRepository.java
@@ -14,10 +14,47 @@
 package edu.vinu.repository;
 
 import edu.vinu.entity.InstituteTeacherEntity;
+import edu.vinu.repository.projection.InstituteTeacherProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InstituteTeacherRepository extends JpaRepository<InstituteTeacherEntity, Long> {
     boolean existsByTeacherIdAndInstituteId(Long teacherId, Long instituteId);
+
+    @Query(value = """
+            SELECT
+            ti.id AS id,
+            ti.status AS status,
+            ti.institute_id AS instituteId,
+            ti.joined_date AS joinedDate,
+            ti.last_modified_date As lastModifiedDate,
+            
+            u.id AS userId,
+            u.email AS email,
+            u.contact As contact,
+            u.dp As dp,
+            u.address AS address,
+            
+            t.first_name AS firstName,
+            t.last_name AS lastName,
+            t.dob AS dob
+            
+            FROM institute_teacher ti
+            JOIN teacher t ON ti.teacher_id = t.id
+            JOIN users u ON t.user_id = u.id
+            WHERE ti.institute_id = :instituteId
+            
+            """,
+            countQuery = """
+                    SELECT COUNT(*)
+                    FROM institute_teacher ti
+                    WHERE ti.institute_id = :instituteId
+                    """,
+            nativeQuery = true)
+    Page<InstituteTeacherProjection> getAllByInstituteId(@Param("instituteId") Long instituteId, Pageable pageable);
 }
