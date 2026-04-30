c695f7930fbcd12cc54204cf7575a06148b29469
vinuth <vinuthsriarampath@outlook.com>
2026-04-30 20:25:41 +0530

    feat: add AnnouncementResponse class for handling announcement response data

---
+++ src/main/java/edu/vinu/response/AnnouncementResponse.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.response;
+
+import edu.vinu.enums.AnnouncementStatus;
+import edu.vinu.enums.AnnouncementVisibility;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AnnouncementResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private AnnouncementVisibility visibility;
+    private AnnouncementStatus status;
+    private Boolean isPinned;
+    private LocalDateTime publishedDate;
+    private LocalDateTime expireAt;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+    private Long instituteId;
+    private Long courseId;
+    private Long batchId;
+}
