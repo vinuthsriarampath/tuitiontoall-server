0214c30637ed4b2692386d39b97b5203926dc45c
vinuth <vinuthsriarampath@outlook.com>
2026-05-04 11:38:35 +0530

    feat: add native query to retrieve all announcements of the institute after applying filters with pagination

--- src/main/java/edu/vinu/repository/AnnouncementRepository.java
+++ src/main/java/edu/vinu/repository/AnnouncementRepository.java
@@ -14,9 +14,45 @@
 package edu.vinu.repository;
 
 import edu.vinu.entity.AnnouncementEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnnouncementRepository extends JpaRepository<AnnouncementEntity,Long> {
+public interface AnnouncementRepository extends JpaRepository<AnnouncementEntity, Long> {
+
+    @Query(
+            value =
+                    """
+                            SELECT *
+                            FROM announcements a
+                            WHERE a.institute_id = :instituteId
+                            AND (:visibility IS NULL OR a.visibility = :visibility)
+                            AND (:status IS NULL OR a.status = :status)
+                            AND (:courseId IS NULL OR a.course_id = :courseId)
+                            AND (:batchId IS NULL OR a.batch_id = :batchId)
+                            
+                            """,
+            countQuery =
+                    """
+                            SELECT COUNT(*)
+                            FROM announcements a
+                            WHERE a.institute_id = :instituteId
+                            AND (:visibility IS NULL OR a.visibility = :visibility)
+                            AND (:status IS NULL OR a.status = :status)
+                            AND (:courseId IS NULL OR a.course_id = :courseId)
+                            AND (:batchId IS NULL OR a.batch_id = :batchId)
+                            """,
+            nativeQuery = true)
+    Page<AnnouncementEntity> findAllByInstituteWithFilters(
+            @Param("instituteId") Long instituteId,
+            @Param("visibility") String visibility,
+            @Param("status") String status,
+            @Param("courseId") Long courseId,
+            @Param("batchId") Long batchId,
+            Pageable pageable
+    );
 }
