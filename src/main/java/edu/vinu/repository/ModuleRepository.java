d30038d03a650cff745d8e0f85a7291a8070f73d
vinuth <vinuthsriarampath@outlook.com>
2026-05-12 20:16:08 +0530

    feat: add custom query to retrieve modules with optional status and batchId filtering

--- src/main/java/edu/vinu/repository/ModuleRepository.java
+++ src/main/java/edu/vinu/repository/ModuleRepository.java
@@ -14,10 +14,34 @@
 package edu.vinu.repository;
 
 import edu.vinu.entity.ModuleEntity;
+import edu.vinu.enums.ModuleStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<ModuleEntity, Long> {
     boolean existsByNameAndBatchId(String name, Long batchId);
+
+    @Query(
+            value = """
+                    SELECT *
+                    FROM module m
+                    WHERE(:status IS NULL OR m.status = :status)
+                    AND (:batchId IS NULL OR m.batch_id = :batchId)
+            """,
+            countQuery = """
+                    SELECT COUNT(*)
+                    FROM module m
+                    WHERE(:status IS NULL OR m.status = :status)
+                    AND (:batchId IS NULL OR m.batch_id = :batchId)
+            """,
+            nativeQuery = true
+    )
+    Page<ModuleEntity> getAllModules(Pageable pageable, @Param("status") String status, @Param("batchId") Long batchId);
 }
