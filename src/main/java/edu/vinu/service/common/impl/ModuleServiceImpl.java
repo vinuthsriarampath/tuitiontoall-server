01b12613cd09ff9616d4af7f95320524d03bd7ee
vinuth <vinuthsriarampath@outlook.com>
2026-05-12 15:42:28 +0530

    feat: implement createModule method in ModuleServiceImpl with validation and mapping

--- src/main/java/edu/vinu/service/common/impl/ModuleServiceImpl.java
+++ src/main/java/edu/vinu/service/common/impl/ModuleServiceImpl.java
@@ -13,17 +13,82 @@
 
 package edu.vinu.service.common.impl;
 
+import edu.vinu.entity.BatchEntity;
+import edu.vinu.entity.ModuleEntity;
+import edu.vinu.enums.ModuleStatus;
+import edu.vinu.exception.custom.InvalidInputException;
+import edu.vinu.repository.ModuleRepository;
 import edu.vinu.request.modules.ModuleCreateRequest;
+import edu.vinu.request.modules.enums.ModuleCreateStatus;
+import edu.vinu.response.FieldError;
 import edu.vinu.response.module.ModuleResponse;
+import edu.vinu.service.common.BatchService;
 import edu.vinu.service.common.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ModuleServiceImpl implements ModuleService {
+    private final ModuleRepository moduleRepository;
+    private final BatchService batchService;
     @Override
     public ModuleResponse createModule(ModuleCreateRequest request) {
-        return null;
+        BatchEntity batchEntity = batchService.getBatchEntityById(request.getBatchId());
+
+        if(!batchService.isBatchOwner(batchEntity)){
+            throw new InvalidInputException("You are not authorized to create module for this batch");
+        }
+
+        List<FieldError> errors = new ArrayList<>();
+
+        if(isModuleExists(request.getName(), request.getBatchId())){
+            errors.add(new FieldError("name","same module name already exists in the batch"));
+        }
+
+        if (!errors.isEmpty()) {
+            throw new InvalidInputException(errors);
+        }
+
+        ModuleEntity moduleEntity = ModuleEntity.builder()
+                .name(request.getName())
+                .batch(batchEntity)
+                .status(mapToModuleStatus(request.getStatus()))
+                .build();
+
+        return mapToAnnouncementResponse(moduleRepository.save(moduleEntity));
+    }
+
+    private boolean isModuleExists(String name, Long batchId){
+        return moduleRepository.existsByNameAndBatchId(name,batchId);
+    }
+
+    private ModuleStatus mapToModuleStatus(ModuleCreateStatus status){
+        switch (status){
+            case DRAFT -> {
+                return ModuleStatus.DRAFT;
+            }
+            case PUBLISHED -> {
+                return ModuleStatus.PUBLISHED;
+            }
+            case LOCKED -> {
+                return ModuleStatus.LOCKED;
+            }
+            default -> throw new IllegalArgumentException("Invalid module status: " + status);
+        }
+    }
+
+    private ModuleResponse mapToAnnouncementResponse(ModuleEntity entity){
+        return ModuleResponse.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .status(entity.getStatus())
+                .batchId(entity.getBatch().getId())
+                .createdDate(entity.getCreatedAt())
+                .lastModifiedDate(entity.getLastModifiedDate())
+                .build();
     }
 }
