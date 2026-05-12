43f4ce27a713607d06e3a413543ca5c7a42737aa
vinuth <vinuthsriarampath@outlook.com>
2026-05-12 08:49:51 +0530

    feat: add ModuleCreateRequest, ModuleCreateStatus, and ModuleResponse classes for module management

---
+++ src/main/java/edu/vinu/request/modules/ModuleCreateRequest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.request.modules;
+
+import edu.vinu.request.modules.enums.ModuleCreateStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ModuleCreateRequest {
+    private String name;
+    private ModuleCreateStatus status;
+    private Long batchId;
+}
