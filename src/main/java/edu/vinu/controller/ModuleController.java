45571b8aca853a3ac81fbc5349419d862b82fcb5
vinuth <vinuthsriarampath@outlook.com>
2026-05-12 08:53:55 +0530

    feat: add ModuleController for module API endpoints

---
+++ src/main/java/edu/vinu/controller/ModuleController.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/modules")
+@RequiredArgsConstructor
+public class ModuleController {
+
+}
