ccfb66cb47120a9b5b424947d68ab0239a9dd43e
vinuth <vinuthsriarampath@outlook.com>
2026-05-14 20:13:02 +0530

    feat: add ModuleTeacherUpdateRequest for updating teacher with mandatory teacher ID

---
+++ src/main/java/edu/vinu/request/modules/ModuleTeacherUpdateRequest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.request.modules;
+
+import jakarta.validation.constraints.NotNull;
+
+
+public record ModuleTeacherUpdateRequest(
+        @NotNull(message = "Teacher ID is mandatory!")
+        Long teacherId
+) {
+}
