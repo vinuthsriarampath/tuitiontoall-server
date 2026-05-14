3c1083b48da87dc0c97ab0765c34728d0c38e4ae
vinuth <vinuthsriarampath@outlook.com>
2026-05-14 13:33:00 +0530

    feat: add TeacherBasicResponse class for teacher data representation

---
+++ src/main/java/edu/vinu/response/TeacherBasicResponse.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TeacherBasicResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+}
