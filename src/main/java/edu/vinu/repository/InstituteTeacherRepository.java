d7a73eb5ccd58e4c3758a2fad209b9e6120da456
vinuth <vinuthsriarampath@outlook.com>
2026-05-14 13:33:35 +0530

    feat: add query to retrieve all teachers by institute ID

--- src/main/java/edu/vinu/repository/InstituteTeacherRepository.java
+++ src/main/java/edu/vinu/repository/InstituteTeacherRepository.java
@@ -16,12 +16,15 @@ package edu.vinu.repository;
 import edu.vinu.entity.InstituteTeacherEntity;
 import edu.vinu.repository.projection.InstituteTeacherProjection;
 import edu.vinu.repository.projection.InstituteTeacherStatsProjection;
+import edu.vinu.repository.projection.TeacherProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface InstituteTeacherRepository extends JpaRepository<InstituteTeacherEntity, Long> {
@@ -60,13 +63,27 @@ public interface InstituteTeacherRepository extends JpaRepository<InstituteTeach
     Page<InstituteTeacherProjection> getAllByInstituteId(@Param("instituteId") Long instituteId, Pageable pageable);
 
     @Query(value = """
-    SELECT
-        COUNT(*) AS totalTeachers,
-        SUM(CASE WHEN status = 'ACTIVE' THEN 1 ELSE 0 END) AS activeTeachers,
-        SUM(CASE WHEN status = 'INACTIVE' THEN 1 ELSE 0 END) AS inactiveTeachers,
-        SUM(CASE WHEN status = 'SUSPENDED' THEN 1 ELSE 0 END) AS suspendedTeachers
-     FROM institute_teacher it
-     where it.institute_id = :instituteId
-""",nativeQuery = true)
+                SELECT
+                    COUNT(*) AS totalTeachers,
+                    SUM(CASE WHEN status = 'ACTIVE' THEN 1 ELSE 0 END) AS activeTeachers,
+                    SUM(CASE WHEN status = 'INACTIVE' THEN 1 ELSE 0 END) AS inactiveTeachers,
+                    SUM(CASE WHEN status = 'SUSPENDED' THEN 1 ELSE 0 END) AS suspendedTeachers
+                 FROM institute_teacher it
+                 where it.institute_id = :instituteId
+            """, nativeQuery = true)
     InstituteTeacherStatsProjection getInstituteTeacherStatsByInstituteId(@Param("instituteId") Long instituteId);
+
+    @Query(
+            value = """
+                SELECT
+                    t.id AS teacherId,
+                    t.first_name AS firstName,
+                    t.last_name AS lastName,
+                    t.dob AS dob
+                FROM institute_teacher it
+                JOIN teacher t ON it.teacher_id = t.id
+                WHERE it.institute_id = :instituteId
+            """,nativeQuery = true
+    )
+    List<TeacherProjection> findAllTeachersByInstituteId(@Param("instituteId") Long instituteId);
 }
