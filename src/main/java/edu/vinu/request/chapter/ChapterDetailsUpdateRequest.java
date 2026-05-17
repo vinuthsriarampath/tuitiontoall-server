e2303bf674ed99b24dd9b336dcced3b2a0563cdd
vinuth <vinuthsriarampath@outlook.com>
2026-05-17 20:21:22 +0530

    feat: add ChapterDetailsUpdateRequest for updating chapter details with validation

---
+++ src/main/java/edu/vinu/request/chapter/ChapterDetailsUpdateRequest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.request.chapter;
+
+import edu.vinu.enums.ChapterStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+public record ChapterDetailsUpdateRequest(
+        @NotBlank(message = "Title is mandatory!")
+        String title,
+        @NotNull(message = "Chapter status is mandatory!")
+        ChapterStatus status,
+        @NotNull(message = "Module id is mandatory!")
+        Long moduleId
+) {
+}
