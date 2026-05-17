d44863e917d644de420f60ecc8f8f2516cf7c6fb
vinuth <vinuthsriarampath@outlook.com>
2026-05-17 18:34:25 +0530

    feat: add chapter controller, service and repository classes

---
+++ src/main/java/edu/vinu/controller/ChapterController.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.controller;
+
+import edu.vinu.service.common.ChapterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/chapters")
+@RequiredArgsConstructor
+public class ChapterController {
+    private final ChapterService chapterService;
+}
