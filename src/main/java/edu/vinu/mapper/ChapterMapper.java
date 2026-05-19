87aff3faf3b2da9493083d8ebeebcd2215cd4d45
vinuth <vinuthsriarampath@outlook.com>
2026-05-19 19:35:35 +0530

    feat: add method to convert ChapterDetailedProjection to ChapterDetailedResponse

--- src/main/java/edu/vinu/mapper/ChapterMapper.java
+++ src/main/java/edu/vinu/mapper/ChapterMapper.java
@@ -14,7 +14,10 @@
 package edu.vinu.mapper;
 
 import edu.vinu.entity.ChapterEntity;
+import edu.vinu.repository.projection.ChapterDetailedProjection;
+import edu.vinu.response.chapter.ChapterDetailedResponse;
 import edu.vinu.response.chapter.ChapterResponse;
+import edu.vinu.response.module.ModuleResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -28,6 +31,26 @@ public class ChapterMapper {
                 .status(chapterEntity.getStatus())
                 .createdDate(chapterEntity.getCreatedDate())
                 .lastModifiedDate(chapterEntity.getLastModifiedDate())
+                .build();
+    }
+
+    public static ChapterDetailedResponse toChapterDetailedResponse(ChapterDetailedProjection proj){
+        return ChapterDetailedResponse.builder()
+                .id(proj.getId())
+                .title(proj.getTitle())
+                .status(proj.getStatus())
+                .chapterOrder(proj.getChapterOrder())
+                .createdDate(proj.getCreatedDate())
+                .lastModifiedDate(proj.getLastModifiedDate())
+                .module(ModuleResponse.builder()
+                        .id(proj.getModuleId())
+                        .name(proj.getModuleName())
+                        .status(proj.getModuleStatus())
+                        .batchId(proj.getModuleBatchId())
+                        .teacherId(proj.getModuleTeacherId())
+                        .createdDate(proj.getCreatedDate())
+                        .lastModifiedDate(proj.getLastModifiedDate())
+                        .build())
                 .build();
     }
 }
