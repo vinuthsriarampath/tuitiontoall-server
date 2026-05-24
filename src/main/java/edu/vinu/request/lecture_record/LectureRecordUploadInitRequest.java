fde3821e0d3603f2b5013f2db27bf80dd109b5b2
vinuth <vinuthsriarampath@outlook.com>
2026-05-24 11:58:11 +0530

    feat: add validation constraints to LectureRecordUploadInitRequest fields

--- src/main/java/edu/vinu/request/lecture_record/LectureRecordUploadInitRequest.java
+++ src/main/java/edu/vinu/request/lecture_record/LectureRecordUploadInitRequest.java
@@ -13,14 +13,25 @@
 
 package edu.vinu.request.lecture_record;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.LocalDate;
 
 public record LectureRecordUploadInitRequest(
+        @NotBlank(message = "Title is Mandatory!")
         String title,
+        @NotNull(message = "Recorded Date is Mandatory!")
+        @PastOrPresent(message = "Recorded Date cannot be in the future!")
         LocalDate recordedDate,
+        @NotNull(message = "Chapter ID is Mandatory!")
         Long chapterId,
+        @NotBlank(message = "Original File Name is Mandatory!")
         String originalFileName,
+        @NotNull(message = "Total Size is Mandatory!")
         Long totalSize,
+        @NotNull(message = "Total Chunks is Mandatory!")
         Integer totalChunks
 ) {
 }
