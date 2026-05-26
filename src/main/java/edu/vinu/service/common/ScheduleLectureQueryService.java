813d2c0901ce2d322393775ba92ad58f6c82bde2
vinuth <vinuthsriarampath@outlook.com>
2026-05-26 07:37:46 +0530

    feat: implement ScheduleLectureQueryService and its implementation for retrieving schedule lectures by chapter with pagination and filtering

---
+++ src/main/java/edu/vinu/service/common/ScheduleLectureQueryService.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.service.common;
+
+import edu.vinu.request.schedule_lecture.ScheduleLectureFilterRequest;
+import edu.vinu.response.PaginatedApiResponse;
+import edu.vinu.response.schedule_lecture.ScheduleLectureResponse;
+
+import java.util.List;
+
+public interface ScheduleLectureQueryService {
+    PaginatedApiResponse<ScheduleLectureResponse> getAllScheduleLecturesByChapter(Long chapterId, int page, int size, String direction, List<String> sortBy, ScheduleLectureFilterRequest filters);
+
+}
