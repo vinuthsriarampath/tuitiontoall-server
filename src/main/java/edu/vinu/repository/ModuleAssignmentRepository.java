a3dd0e1f360ee187b2d651611a442873af203572
vinuth <vinuthsriarampath@outlook.com>
2026-06-02 20:34:15 +0530

    feat: add method to check existence of assignment by ID in ModuleAssignmentRepository

--- src/main/java/edu/vinu/repository/ModuleAssignmentRepository.java
+++ src/main/java/edu/vinu/repository/ModuleAssignmentRepository.java
@@ -19,4 +19,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ModuleAssignmentRepository extends JpaRepository<ModuleAssignmentEntity,Long> {
+    boolean existsByAssignmentId(Long assignmentId);
 }
