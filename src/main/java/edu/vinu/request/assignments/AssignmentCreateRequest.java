6827b4ba5d5beb4650f67b1b0e99151440d5a307
vinuth <vinuthsriarampath@outlook.com>
2026-06-04 07:25:47 +0530

    feat: add validation for maxAttempts and gradingRanges in AssignmentCreateRequest

--- src/main/java/edu/vinu/request/assignments/AssignmentCreateRequest.java
+++ src/main/java/edu/vinu/request/assignments/AssignmentCreateRequest.java
@@ -14,6 +14,7 @@
 package edu.vinu.request.assignments;
 
 import edu.vinu.request.grading_range.GradingRangeCreateRequest;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 
@@ -22,22 +23,34 @@ import java.util.List;
 
 @Builder
 public record AssignmentCreateRequest(
+
         @NotBlank(message = "Assignment topic is mandatory!")
         String topic,
+
         String description,
+
         @NotNull(message = "Total mark is mandatory!")
         Integer totalMarks,
+
         @NotNull(message = "File name is mandatory!")
         String fileName,
+
         @NotNull(message = "Assignment public available date is mandatory!")
         @FutureOrPresent(message = "Assignment public available date must be in the present or future!")
         LocalDateTime availableOn,
+
         @NotNull(message = "Assignment due date is mandatory!")
         @Future(message = "Assignment due date must be in the future!")
         LocalDateTime dueDate,
+
         boolean lateSubmission,
         boolean resubmission,
+
+        @NotNull(message = "Max attempts is mandatory!")
+        @Min(value = 1, message = "Minimum value for Max attempts is 1!")
         Integer maxAttempts,
+
+        @Valid
         @NotNull(message = "Grading ranges list is mandatory!")
         @NotEmpty(message = "At least one grading range is required!")
         List<GradingRangeCreateRequest> gradingRanges
