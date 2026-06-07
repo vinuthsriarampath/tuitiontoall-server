ac8bb12598269b96cbdf21222ee507f7bea48762
vinuth <vinuthsriarampath@outlook.com>
2026-06-07 18:57:23 +0530

    refactor: rename BatchBasicResponse and update package structure to edu.vinu.domain.batch.response

--- src/main/java/edu/vinu/domain/batch/response/BatchBasicResponse.java
+++ src/main/java/edu/vinu/domain/batch/response/BatchBasicResponse.java
@@ -11,7 +11,7 @@
  *
  */
 
-package edu.vinu.response;
+package edu.vinu.domain.batch.response;
 
 import edu.vinu.domain.batch.enums.BatchEnrollmentStatus;
 import edu.vinu.domain.batch.enums.BatchStatus;
