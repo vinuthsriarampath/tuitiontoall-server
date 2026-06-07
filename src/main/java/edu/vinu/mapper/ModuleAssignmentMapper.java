a70f6deb54dd52e792fad63306a1aca228a971b6
vinuth <vinuthsriarampath@outlook.com>
2026-06-07 08:48:49 +0530

    feat: add ModuleAssignmentMapper for converting between ModuleAssignmentEntity and ModuleAssignmentResponse

---
+++ src/main/java/edu/vinu/mapper/ModuleAssignmentMapper.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.mapper;
+
+import edu.vinu.entity.*;
+import edu.vinu.response.assignments.module_assignment.ModuleAssignmentResponse;
+
+public class ModuleAssignmentMapper {
+    public static ModuleAssignmentEntity toModuleAssignmentEntity(AssignmentEntity assignmentEntity, ModuleEntity moduleEntity) {
+        return ModuleAssignmentEntity.builder()
+                .assignment(assignmentEntity)
+                .module(moduleEntity)
+                .build();
+    }
+
+    public static ModuleAssignmentResponse toModuleAssignmentResponse(ModuleAssignmentEntity entity) {
+        return ModuleAssignmentResponse.builder()
+                .id(entity.getId())
+                .assignmentId(entity.getAssignment().getId())
+                .moduleId(entity.getModule().getId())
+                .topic(entity.getAssignment().getTopic())
+                .description(entity.getAssignment().getDescription())
+                .fileName(entity.getAssignment().getFileName())
+                .totalMarks(entity.getAssignment().getTotalMarks())
+                .availableOn(entity.getAssignment().getAvailableOn())
+                .dueDate(entity.getAssignment().getDueDate())
+                .lateSubmission(entity.getAssignment().isLateSubmission())
+                .reSubmission(entity.getAssignment().isResubmission())
+                .maxAttempts(entity.getAssignment().getMaxAttempts())
+                .createdDate(entity.getAssignment().getCreatedDate())
+                .lastModifiedDate(entity.getAssignment().getLastModifiedDate())
+                .build();
+    }
+
+}
