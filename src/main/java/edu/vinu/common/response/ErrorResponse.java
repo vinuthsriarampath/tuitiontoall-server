8dbdff6f911b74d2baf8db47bd7b06ca4a8d512b
vinuth <vinuthsriarampath@outlook.com>
2026-06-07 16:42:41 +0530

    refactor: rename ErrorResponse.java and update package structure to common.response

---
+++ src/main/java/edu/vinu/common/response/ErrorResponse.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.common.response;
+
+import edu.vinu.common.dto.FieldError;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private String message;
+    private int status;
+    private List<FieldError> errors;
+    private LocalDateTime timestamp;
+}
