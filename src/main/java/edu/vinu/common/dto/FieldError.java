71960aebb27db4a920d650618835dfaba7ed5e52
vinuth <vinuthsriarampath@outlook.com>
2026-06-07 16:38:28 +0530

    refactor: moved the FieldError.java to dto package inside common package

---
+++ src/main/java/edu/vinu/common/dto/FieldError.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FieldError {
+    private String field;
+    private String message;
+}
