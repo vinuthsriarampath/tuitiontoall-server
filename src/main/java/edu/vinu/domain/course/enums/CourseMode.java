ba8eec49f06e77a5e5de47c3175db1a2b0f37917
vinuth <vinuthsriarampath@outlook.com>
2026-06-07 18:13:38 +0530

    refactor: rename enum files and update package structure to edu.vinu.domain.course.enums

--- src/main/java/edu/vinu/domain/course/enums/CourseMode.java
+++ src/main/java/edu/vinu/domain/course/enums/CourseMode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 vinuth sri arampath
+ * Copyright (c) 2026 vinuth sri arampath
  *
  * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
  * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
@@ -11,10 +11,10 @@
  *
  */
 
-package edu.vinu.enums;
+package edu.vinu.domain.course.enums;
 
-public enum CourseLevel {
-    BEGINNER,
-    INTERMEDIATE,
-    ADVANCED
+public enum CourseMode {
+    ONLINE,
+    OFFLINE,
+    HYBRID
 }
--- src/main/java/edu/vinu/domain/course/enums/CourseStatus.java
+++ src/main/java/edu/vinu/domain/course/enums/CourseStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 vinuth sri arampath
+ * Copyright (c) 2026 vinuth sri arampath
  *
  * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
  * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
@@ -11,15 +11,10 @@
  *
  */
 
-package edu.vinu.enums;
+package edu.vinu.domain.course.enums;
 
-public enum CourseCategory {
-    SCIENCE,
-    TECHNOLOGY,
-    ENGINEERING,
-    MATHEMATICS,
-    BUSINESS,
-    ARTS,
-    LANGUAGE,
-    OTHER
+public enum CourseStatus {
+    DRAFT,
+    PUBLISHED,
+    ARCHIVED
 }
