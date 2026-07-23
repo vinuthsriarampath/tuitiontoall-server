c245935010d36fcbf341671688532a71f57ae494
vinuth <vinuthsriarampath@outlook.com>
2026-07-23 21:03:58 +0530

    refactor: remove unused import for LocalDateTime in TeacherVacancyRepository

--- src/main/java/edu/vinu/domain/teacher_vacancy/repository/TeacherVacancyRepository.java
+++ src/main/java/edu/vinu/domain/teacher_vacancy/repository/TeacherVacancyRepository.java
@@ -24,7 +24,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
