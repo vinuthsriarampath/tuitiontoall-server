e523a3fee3e36a367afd8f647538fd821f9165e5
vinuth <vinuthsriarampath@outlook.com>
2026-08-18 10:34:13 +0530

    feat: add a method with pagination support to retrieve feedbacks by course

--- src/main/java/edu/vinu/domain/feedback/repository/FeedbackRepository.java
+++ src/main/java/edu/vinu/domain/feedback/repository/FeedbackRepository.java
@@ -14,6 +14,8 @@
 package edu.vinu.domain.feedback.repository;
 
 import edu.vinu.domain.feedback.entity.Feedback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -27,4 +29,16 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     WHERE f.course_id = :courseId  AND f.user_id = :userId
     """, nativeQuery = true)
     int countUserFeedbacksByCourseId(Long courseId, Long userId);
+
+    @Query(value = """
+    SELECT *
+        FROM feedbacks f
+        WHERE f.course_id = :courseId
+    """,
+    countQuery = """
+    SELECT COUNT(*)
+        FROM feedbacks f
+        WHERE f.course_id = :courseId
+    """, nativeQuery = true)
+    Page<Feedback> getFeedbacksByCourse(Long courseId, Pageable pageable);
 }
