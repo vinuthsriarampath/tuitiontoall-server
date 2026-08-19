08e3f87b994bb708d02df692abb4784d8e5d9407
vinuth <vinuthsriarampath@outlook.com>
2026-08-19 10:38:15 +0530

    feat: add CourseStatsResponse class to encapsulate course statistics data

---
+++ src/main/java/edu/vinu/domain/course/response/CourseStatsResponse.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.domain.course.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseStatsResponse{
+    private Long courseId;
+    private Long activeStudents = 0L;
+    private Long ongoingBatches = 0L;
+    private BigDecimal totalRevenue = BigDecimal.ZERO;
+}
