4535aadcc97ccfa265818b88a2e290d4722b7050
vinuth <vinuthsriarampath@outlook.com>
2026-08-19 10:37:48 +0530

    feat: add query to sum revenue by course and payment status in PaymentRepository

--- src/main/java/edu/vinu/domain/payment/repository/PaymentRepository.java
+++ src/main/java/edu/vinu/domain/payment/repository/PaymentRepository.java
@@ -14,9 +14,23 @@
 package edu.vinu.domain.payment.repository;
 
 import edu.vinu.domain.payment.entity.Payment;
+import edu.vinu.domain.payment.enums.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    @Query(value = """
+    SELECT COALESCE(SUM(p.amount), 0)
+        FROM payment p
+            INNER JOIN student_batch_enrollment sbe ON sbe.payment_id = p.id
+            INNER JOIN batch b ON sbe.batch_id = b.id
+        WHERE b.course_id = :courseId
+        AND p.payment_status =:paymentStatus
+    """, nativeQuery = true)
+    BigDecimal sumRevenueByCourseId(Long courseId, PaymentStatus paymentStatus);
 }
