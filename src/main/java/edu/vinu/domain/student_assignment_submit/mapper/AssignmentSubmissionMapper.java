3b59f758749b106098a4f6efe38c306841e93970
vinuth <vinuthsriarampath@outlook.com>
2026-09-17 12:03:10 +0530

    feat: add AssignmentSubmissionMapper and AssignmentSubmissionResponse for submission handling

---
+++ src/main/java/edu/vinu/domain/student_assignment_submit/mapper/AssignmentSubmissionMapper.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.domain.student_assignment_submit.mapper;
+
+import edu.vinu.domain.student_assignment_submit.entity.StudentAssignmentSubmit;
+import edu.vinu.domain.student_assignment_submit.response.AssignmentSubmissionResponse;
+
+public class AssignmentSubmissionMapper {
+
+    public static AssignmentSubmissionResponse toAssignmentSubmissionResponse(StudentAssignmentSubmit e){
+        return AssignmentSubmissionResponse.builder()
+                .submissionId(e.getId())
+                .studentId(e.getStudent().getId())
+                .assignmentId(e.getAssignment().getId())
+                .url(e.getUrl())
+                .grade(e.getGrade())
+                .status(e.getStatus())
+                .marksGained(e.getMarksGained())
+                .attemptNo(e.getAttemptNo())
+                .submittedAt(e.getSubmittedAt())
+                .lastModifiedDate(e.getLastModifiedDate())
+                .build();
+    }
+}
