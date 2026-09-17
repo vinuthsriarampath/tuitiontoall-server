9fd90e3f7b0d3a1b9aa5f4fcd58662e476fa937d
vinuth <vinuthsriarampath@outlook.com>
2026-09-17 12:07:48 +0530

    feat: add endpoints for assignment submission and eligibility check

--- src/main/java/edu/vinu/domain/student_assignment_submit/controller/AssignmentSubmitController.java
+++ src/main/java/edu/vinu/domain/student_assignment_submit/controller/AssignmentSubmitController.java
@@ -13,16 +13,31 @@
 
 package edu.vinu.domain.student_assignment_submit.controller;
 
+import edu.vinu.common.response.ApiResponse;
 import edu.vinu.domain.student_assignment_submit.service.AssignmentSubmitService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v2/assignment-submissions/{assignmentId}")
+@RequestMapping("/api/v2/assignment-submissions")
 @RequiredArgsConstructor
 public class AssignmentSubmitController {
 
     private final AssignmentSubmitService submitService;
 
+
+    @PreAuthorize("hasAuthority('student')")
+    @PostMapping("/assignment/{assignmentId}")
+    public ResponseEntity<ApiResponse> submit(@PathVariable Long assignmentId, @RequestParam("file")MultipartFile file) {
+        return ResponseEntity.ok(submitService.submit(assignmentId, file));
+    }
+
+    @PreAuthorize("hasAuthority('student')")
+    @GetMapping("/assignment/{assignmentId}/eligibility")
+    public ResponseEntity<ApiResponse> checkEligibility(@PathVariable Long assignmentId) {
+        return ResponseEntity.ok(submitService.checkEligibility(assignmentId));
+    }
 }
