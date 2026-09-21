44bb09d7d499c40d99367ef2142749e8ef4b806b
vinuth <vinuthsriarampath@outlook.com>
2026-09-21 14:07:33 +0530

    feat: implement myPayments method in PaymentService for retrieving paginated student payment details with filters

--- src/main/java/edu/vinu/domain/payment/service/PaymentService.java
+++ src/main/java/edu/vinu/domain/payment/service/PaymentService.java
@@ -13,7 +13,11 @@
 
 package edu.vinu.domain.payment.service;
 
+import edu.vinu.common.dto.PaginationRequest;
+import edu.vinu.common.response.PaginatedApiResponse;
 import edu.vinu.domain.institute.entity.InstituteEntity;
+import edu.vinu.domain.payment.dto.request.MyPaymentFilterRequest;
+import edu.vinu.domain.payment.dto.response.PaymentDetailedResponse;
 import edu.vinu.domain.payment.entity.Payment;
 import edu.vinu.domain.student.entity.StudentEntity;
 
@@ -21,4 +25,6 @@ import java.math.BigDecimal;
 
 public interface PaymentService {
     Payment pay(BigDecimal amount, StudentEntity studentEntity, InstituteEntity instituteEntity);
+
+    PaginatedApiResponse<PaymentDetailedResponse> myPayments(PaginationRequest pagination, MyPaymentFilterRequest filters);
 }
--- src/main/java/edu/vinu/domain/payment/service/impl/PaymentServiceImpl.java
+++ src/main/java/edu/vinu/domain/payment/service/impl/PaymentServiceImpl.java
@@ -13,24 +13,36 @@
 
 package edu.vinu.domain.payment.service.impl;
 
+import edu.vinu.common.dto.PaginationRequest;
 import edu.vinu.common.exception.custom.InvalidInputException;
+import edu.vinu.common.response.PaginatedApiResponse;
+import edu.vinu.common.util.SortUtil;
 import edu.vinu.domain.institute.entity.InstituteEntity;
+import edu.vinu.domain.payment.dto.request.MyPaymentFilterRequest;
+import edu.vinu.domain.payment.dto.response.PaymentDetailedResponse;
 import edu.vinu.domain.payment.entity.Payment;
 import edu.vinu.domain.payment.enums.PaymentMethod;
 import edu.vinu.domain.payment.enums.PaymentStatus;
+import edu.vinu.domain.payment.mapper.PaymentMapper;
 import edu.vinu.domain.payment.repository.PaymentRepository;
 import edu.vinu.domain.payment.service.PaymentService;
 import edu.vinu.domain.student.entity.StudentEntity;
+import edu.vinu.domain.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentRepository paymentRepository;
+    private final StudentService studentService;
     @Override
     public Payment pay(BigDecimal amount, StudentEntity studentEntity, InstituteEntity instituteEntity) {
         Payment paymentEntity = Payment.builder()
@@ -46,5 +58,34 @@ public class PaymentServiceImpl implements PaymentService {
         }catch (DataIntegrityViolationException e) {
             throw new InvalidInputException("Duplicate Payment!");
         }
+    }
+
+    @Override
+    public PaginatedApiResponse<PaymentDetailedResponse> myPayments(PaginationRequest pagination, MyPaymentFilterRequest filters) {
+        StudentEntity currentStudent = studentService.getCurrentStudent();
+
+        Pageable pageable = PageRequest.of(pagination.page(), pagination.size(), SortUtil.buildSort(pagination.direction(),pagination.sortBy(), List.of("created_date")));
+
+        Page<PaymentDetailedResponse> page = paymentRepository.getMyPayments(
+                currentStudent.getId(),
+                filters.id(),
+                filters.instituteId(),
+                filters.instituteName(),
+                filters.status() != null ? filters.status().name() : null,
+                filters.paymentMethod() != null ? filters.paymentMethod().name() : null,
+                filters.transactionRef(),
+                filters.createdDate(),
+                pageable
+        ).map(PaymentMapper::toPaymentDetailedResponse);
+
+        return PaginatedApiResponse.<PaymentDetailedResponse>builder()
+                .message("Payments retrieved successfully")
+                .data(page.getContent())
+                .page(page.getNumber())
+                .size(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .last(page.isLast())
+                .build();
     }
 }
