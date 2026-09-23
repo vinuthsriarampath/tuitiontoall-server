2eedaad447f1e3ac21ad44f9ae41d10902410d6d
vinuth <vinuthsriarampath@outlook.com>
2026-09-23 10:24:54 +0530

    feat: implement StudentStatService and StudentStatServiceImpl for student statistics

---
+++ src/main/java/edu/vinu/domain/student/service/impl/StudentStatServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2026 vinuth sri arampath
+ *
+ * This code is the intellectual property of vinuth sri arampath and is protected under copyright law.
+ * Unauthorized copying, modification, distribution, or use of this code, in whole or in part,
+ * without prior written permission is strictly prohibited.
+ *
+ * Portions of this code may be generated with AI and modified by vinuth sri arampath
+ * All rights reserved.
+ *
+ *
+ */
+
+package edu.vinu.domain.student.service.impl;
+
+import edu.vinu.common.response.DashboardStats;
+import edu.vinu.domain.student.service.StudentStatService;
+import edu.vinu.domain.student_batch_enrollment.service.EnrollmentStatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class StudentStatServiceImpl implements StudentStatService {
+    private final EnrollmentStatService enrollmentStatService;
+    @Override
+    public DashboardStats getTotalEnrollmentsStats(Long studentId) {
+        return DashboardStats.builder()
+                .value(BigDecimal.valueOf(enrollmentStatService.getStudentCourseEnrollmentCount(studentId)))
+                .label("Total Enrollments")
+                .build();
+    }
+
+    @Override
+    public DashboardStats getPendingAssignmentsStats(Long studentId) {
+        // TODO : Implement logic to fetch pending assignments stats for the student
+        return null;
+    }
+
+    @Override
+    public DashboardStats getCompletedAssignmentsStats(Long studentId) {
+        // TODO : Implement logic to fetch completed assignments stats for the student
+        return null;
+    }
+
+    @Override
+    public DashboardStats getAverageMarksStats(Long studentId) {
+        // TODO : Implement logic to fetch average marks stats for the student
+        return null;
+    }
+}
